13d6f04c3ac5ab3f071c3555f793d7d161e8ae2f
Lukas Grossberger <code@grossberger.xyz>
2025-03-31 18:27:16 +0200

    fix local scene calendar timezones

--- automation/main.py
+++ automation/main.py
@@ -8,7 +8,7 @@ import requests
 HERE = Path(__file__).parent
 
 
-def main(sheet_csv_url: str, output_yaml_file_path: Path):
+def main(sheet_csv_url: str, output_yaml_file_path: Path, time_zone: str):
     # Google Sheet CSV URL
     response = requests.get(sheet_csv_url)
     response.raise_for_status()
@@ -17,21 +17,26 @@ def main(sheet_csv_url: str, output_yaml_file_path: Path):
     df = pd.read_csv(
         StringIO(response.text),
         usecols=["Title", "Start", "End", "Location", "Added", "Link"],
+        parse_dates=["Start", "End"],
     )
     df = df.rename(columns={c: c.lower() for c in df.columns})
 
     df = df.fillna("")
 
-    df["start"] = pd.to_datetime(df["start"]).dt.strftime("%Y-%m-%dT%H:%M:%S")
-    df["end"] = pd.to_datetime(df["end"]).dt.strftime("%Y-%m-%dT%H:%M:%S")
-
-    # # Extract year and group by it
-    # df["year"] = pd.to_datetime(df["start"], errors="coerce").dt.year
-    # grouped_data = (
-    #     df.groupby("year")
-    #     .apply(lambda x: x.drop(columns=["year"]).to_dict(orient="records"))
-    #     .to_dict()
-    # )
+    df["start_utc"] = (
+        df["start"]
+        .dt.tz_localize(time_zone)
+        .dt.tz_convert("UTC")
+        .dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    )
+    df["start"] = df["start"].dt.strftime("%Y-%m-%dT%H:%M:%S")
+    df["end_utc"] = (
+        df["end"]
+        .dt.tz_localize(time_zone)
+        .dt.tz_convert("UTC")
+        .dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    )
+    df["end"] = df["end"].dt.strftime("%Y-%m-%dT%H:%M:%S")
 
     yaml_output = yaml.dump(
         df.to_dict(orient="records"), sort_keys=False, allow_unicode=True
@@ -41,15 +46,28 @@ def main(sheet_csv_url: str, output_yaml_file_path: Path):
 
 if __name__ == "__main__":
     errors = False
-    for name, url in {
-        "darmstadt": "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Darmstadt",
-        "frankfurt": "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Frankfurt",
-        "heidelberg": "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Heidelberg",
-    }.items():
+    for name, time_zone, url in [
+        (
+            "darmstadt",
+            "Europe/Berlin",
+            "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Darmstadt",
+        ),
+        (
+            "frankfurt",
+            "Europe/Berlin",
+            "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Frankfurt",
+        ),
+        (
+            "heidelberg",
+            "Europe/Berlin",
+            "https://docs.google.com/spreadsheets/d/1xH5iBL0r9ex9bE934b-IPXQeCnpiV-oxsgWJVqLrzCY/gviz/tq?tqx=out:csv&sheet=Heidelberg",
+        ),
+    ]:
         try:
             main(
                 sheet_csv_url=url,
                 output_yaml_file_path=HERE.parent / "data" / f"{name}.yaml",
+                time_zone=time_zone,
             )
         except Exception as e:
             print(f"Error for ${name}:", e)
